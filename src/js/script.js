421a61c005ebd8a307bda77658303488b1e7dda9
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-01-01 21:23:24 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+// Load Footer
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.log('Error loading the footer:', error));
+        
+});
