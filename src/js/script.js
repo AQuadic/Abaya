4e9d7feb929b488432bf800a8e08414e9d4a85be
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-01-06 22:18:48 +0200

    Checkout&Payment Page .. Add nav&footer in components

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,27 @@
+// Load Navbar
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('../../src/Components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the navbar:', error));
+});
+
+// Load Offer
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('../../src/Components/offerBar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('offer').innerHTML = data;
+        })
+        .catch(error => console.log('Error loading the footer:', error));
+        
+});
+
 // Load Footer
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('footer.html')
+    fetch('../../src/Components/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
